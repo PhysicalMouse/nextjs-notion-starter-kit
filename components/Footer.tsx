bc0c375e4fb8bc35ecf90a7d61f428ca3de60971
PhysicalMouse <45204810+PhysicalMouse@users.noreply.github.com>
2026-07-09 15:35:47 +0000

    删除copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,15 +5,9 @@ import * as config from '@/lib/config'
 import styles from './styles.module.css'
 
 export function FooterImpl({ comments }: { comments?: React.ReactNode }) {
-  const currentYear = new Date().getFullYear()
-
   return (
     <footer className={styles.footer}>
       {comments && <div className={styles.comments}>{comments}</div>}
-
-      <div className={styles.copyright}>
-        Copyright {currentYear} {config.author}
-      </div>
     </footer>
   )
 }
