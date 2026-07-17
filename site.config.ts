ece43f152de4ed53222047e416b4bc5fb1686255
PhysicalMouse <45204810+PhysicalMouse@users.noreply.github.com>
2026-07-17 08:03:20 +0000

    redis

--- site.config.ts
+++ site.config.ts
@@ -44,7 +44,7 @@ export default siteConfig({
 
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: set the REDIS_URL environment variable with your connection string
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
