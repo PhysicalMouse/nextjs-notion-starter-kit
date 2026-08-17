19c5a0162490feefc43152ddcb8f12f1ffcee14e
PhysicalMouse <45204810+PhysicalMouse@users.noreply.github.com>
2026-08-17 14:32:32 +0000

    测试

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -13,7 +13,7 @@ export const notion = new NotionAPI({
     timeout: 30_000
     headers: {
       'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
     },
   }
 })
